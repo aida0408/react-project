fb5381c833a716938ac7be9b8e229697920c46e8
Bakazova Aida <solnyshko669@gmail.com>
2021-08-18 01:56:37 +0600

    novoe

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,12 @@ import MainItem from "./MainItem"
 
 function Main(){
     <div className="main">
-        
+        <div className="container">
+            <h2 className="main-title">Shop</h2>
+            <p className="main-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <MainItem />
+        </div>
     </div>
 }
 
